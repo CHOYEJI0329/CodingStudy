cb0caeff4d0fc370c22fc55c9b6d2e64b0d079b4
choyeji0329 <choyeji0329@gmail.com>
2022-01-28 17:15:11 +0900

    [20220128] BOJ - Num15655, Num15656, Num15657 COMMIT.

---
+++ BOJ/basic2/src/Num15656.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Num15656 {
+
+    static int[] a; static int[] c;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        a = new int[M]; c = new int[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            c[i] = Integer.parseInt(st2.nextToken());
+        }
+        Arrays.sort(c);
+        go(0, N, M);
+
+        System.out.println(sb);
+
+    }
+
+    public static void go(int index, int N, int M){
+
+        if(index == M){
+            for(int i = 0; i < M; i++){
+                sb.append(a[i]);
+                if(i != M-1) sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            a[index] = c[i];
+            go(index+1, N, M);
+        }
+
+
+    }
+
+}
